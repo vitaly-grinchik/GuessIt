bd141ac6bb5659e76f903dbd6cd11da0eb03dd43
Vitaly Grinchik <vitaly.grinchik@gmail.com>
2023-08-05 15:31:54 +0300

    Initial Commit

---
+++ GuessIt/GuessItApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessItApp.swift
+//  GuessIt
+//
+//  Created by Виталий Гринчик on 5.08.23.
+//
+
+import SwiftUI
+
+@main
+struct GuessItApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
